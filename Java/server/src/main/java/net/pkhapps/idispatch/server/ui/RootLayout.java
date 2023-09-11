83a120175643cf54cc5025810e6d2d30007af3c2
Petter Holmström <petter.holmstrom@outlook.com>
2023-09-11 19:47:19 +0300

    Add logout

--- Java/server/src/main/java/net/pkhapps/idispatch/server/ui/RootLayout.java
+++ Java/server/src/main/java/net/pkhapps/idispatch/server/ui/RootLayout.java
@@ -2,14 +2,19 @@ package net.pkhapps.idispatch.server.ui;
 
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.AfterNavigationEvent;
 import com.vaadin.flow.router.AfterNavigationObserver;
 import com.vaadin.flow.router.RouterLayout;
+import com.vaadin.flow.spring.security.AuthenticationContext;
 import net.pkhapps.idispatch.server.ui.dws.DispatchView;
 import net.pkhapps.idispatch.server.ui.management.MunicipalityManagementView;
 import net.pkhapps.idispatch.server.ui.reports.ReportsView;
@@ -19,7 +24,7 @@ public class RootLayout extends AppLayout implements RouterLayout, AfterNavigati
     private final HorizontalLayout navbarLayout;
     private final H2 viewTitle;
 
-    public RootLayout() {
+    public RootLayout(AuthenticationContext authenticationContext) {
         var toggle = new DrawerToggle();
         var title = new H1("iDispatch");
         title.getStyle()
@@ -27,9 +32,21 @@ public class RootLayout extends AppLayout implements RouterLayout, AfterNavigati
                 .set("line-height", "var(--lumo-size-l)")
                 .set("margin", "0 var(--lumo-space-m)");
 
+        var logout = new Button(VaadinIcon.POWER_OFF.create(), event -> authenticationContext.logout());
+        var user = new HorizontalLayout(new Span(authenticationContext.getPrincipalName().orElse("")), logout);
+        user.setJustifyContentMode(FlexComponent.JustifyContentMode.BETWEEN);
+        user.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
+        user.setWidthFull();
+        user.setPadding(true);
+
+        var header = new VerticalLayout(title, user);
+        header.setSizeUndefined();
+        header.setPadding(false);
+        header.setSpacing(false);
+
         var views = getPrimaryNavigation();
 
-        addToDrawer(title, views);
+        addToDrawer(header, views);
 
         viewTitle = new H2();
         viewTitle.getStyle()
@@ -56,6 +73,7 @@ public class RootLayout extends AppLayout implements RouterLayout, AfterNavigati
                 TabUtils.createNavigationTab(VaadinIcon.HEADSET, "Dispatcher", DispatchView.class),
                 TabUtils.createNavigationTab(VaadinIcon.COG, "Management", MunicipalityManagementView.class),
                 TabUtils.createNavigationTab(VaadinIcon.CHART, "Reports", ReportsView.class)
+                // TODO Change password
         );
         tabs.setOrientation(Tabs.Orientation.VERTICAL);
         tabs.setSelectedIndex(1);
@@ -70,6 +88,7 @@ public class RootLayout extends AppLayout implements RouterLayout, AfterNavigati
                 .map(HasNavbarContent.class::cast)
                 .findFirst()
                 .ifPresentOrElse(this::setNavbarContent, this::clearNavbarContent);
+        // TODO Select correct tab
     }
 
     private void setNavbarContent(HasNavbarContent contentProvider) {
